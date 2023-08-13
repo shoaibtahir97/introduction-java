a771c53fdba53e0bc65a64e997718907932f51d0
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-08-13 18:38:27 +0500

    computed the area of circle

--- introduction-java/chapter02.java
+++
@@ -1,6 +0,0 @@
-public class chapter02 {
-	public static void main(String args[]) {
-		System.out.println("First Commit");
-	}
-
-}
---
+++ introduction-java/chapter02/ComputeArea.java
@@ -0,0 +1,13 @@
+package chapter02;
+public class ComputeArea {
+	public static void main(String args[]) {
+		double radius;
+		double area;
+		//Find radius of circle
+		radius = 10;
+		//Compute area of circle
+		area = radius * radius * 3.142;
+		//Display the area to console
+		System.out.println("The area of the circle with radius " + radius + " is " + area);
+	}
+}
