65b828069f9f1aa6c50451890f44c8c6b7af7fa8
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-08-13 22:45:25 +0500

    Did two exercises from chapter 01

---
+++ introduction-java/chapter01/programmingExercises.java
@@ -0,0 +1,25 @@
+package chapter01;
+
+public class programmingExercises {
+//Exercise 1.1
+	/*
+	public static void main(String args[]) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	
+	}
+	*/
+	
+//Exercise 1.2
+	/*
+	public static void main(String args[]) {
+		for(int i = 0; i <=5;i++) {
+			System.out.print("Welcome to Java "+ i + "\n" );
+		}
+	}
+	*/
+	
+//Exercise 1.3
+
+}
